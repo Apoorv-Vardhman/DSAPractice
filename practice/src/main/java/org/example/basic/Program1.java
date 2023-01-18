dfbf287af00664e07418c2bba5b063666ad6e58e
Apoorv-Vardhman <apoorv.vardhman@gmail.com>
2023-01-18 07:05:24 +0530

    running notes added

---
+++ practice/src/main/java/org/example/basic/Program1.java
@@ -0,0 +1,12 @@
+package org.example.basic;
+
+import java.util.Scanner;
+
+// 01. max of two numbers
+public class Program1 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter first number ");
+
+    }
+}
